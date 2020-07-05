8642313f12e0f6fc9267052c8e7631261850bbe0
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2020-07-05 07:49:06 +0530

    Starting with the courcode.py script

---
+++ sourcecode.py
@@ -0,0 +1,21 @@
+print("Hi, my name is Vico and I'll be your Virtual Coffee maker for the day. I hope you day has been pleasent so far? (Yes/No)")
+response = input()
+
+if response == 'Yes' or response == 'yes':
+	print("So good to hear that!")
+
+else:
+	print("I hope my coffee will make you feel better")
+
+print("\n")
+
+print("So, what can I make for you today, you caan choose from the menu below.")
+print("\n")
+print("Pick you poison: ".center(40, '-'))
+printing_menu()
+deciding_coffee()
+
+def printing_menu():
+	print("1. Goth Latte")
+
+def deciding_coffee():
