20ca57d7a1a1bf3669f4aeaedc29ddb7406d53e2
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2020-07-10 17:52:10 +0530

    Updating the source code to align with the changes in a menu

--- sourcecode.py
+++ sourcecode.py
@@ -14,7 +14,7 @@ def response():
     if initial_response == 'Yes':
         print("So good to hear that!")
 	elif initial_response == 'Hell, Yeah!':
-		print("OMG! you sound like someone who just won a lottery! Would you be up for some recommendations to keep that energy up?")
+		print("OMG! You sound like someone who just won a lottery! Would you be up for some recommendations to keep that energy up?")
 		recomm_response = input()
 	else:
         print("I hope my coffee will make you feel better")
@@ -26,14 +26,26 @@ print("\n")
 
 printing_menu()
 
+print("Enter the number prefixed with your choice of coffee: ")
+choice_of_coffee = int(input())
+
 def recommendations():
+	print("Enter the number prefixed with your choice of coffee: ")
+	choice_of_coffee = int(input())
 	switcher_1 = {
-		1: ""
+		1: "Potence Affagato",
+		2: "An Affair with Death",
+		3: "Dope-Dope Doppio",
+		4: "Bull Irish",
+		5: "Evil Latte",
+		6: "Macho Macchiato",
+		7: "Complex Lungo",
+		8: "Bull's Eye"			# Red Eye
 	}
 
 
 def deciding_coffee():
-	switcher = {
+	switcher_2 = {
 		1: "Goth Latte",
 		2: "Indeh Latte",
 		3: "A Sweet little rain",
@@ -48,4 +60,12 @@ def deciding_coffee():
 		12: "Caffe Latte",
 		13: "Cappuccino",
 		14: "Flat White",
+		15: "Potence Affagato",
+		16: "An Affair with Death",
+		17: "Dope-Dope Doppio",
+		18: "Bull Irish",
+		19: "Evil Latte",
+		20: "Macho Macchiato",
+		21: "Complex Lungo",
+		22: "Bull's Eye"
 	}
