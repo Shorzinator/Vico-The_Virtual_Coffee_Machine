b7295f62f133f778226945676b213381a56dfe4a
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2020-07-23 18:09:49 +0530

    Edited out redundant features

--- shapes.py
+++ shapes.py
@@ -3,58 +3,57 @@
 
 import turtle
 
-def creating_turtle():
-	marco = turtle.Turtle()
-	marco.shape("turtle")
-	marco.speed(5)
 
 def draw_circle(turtle, color, size, x, y):
-  turtle.penup()
-  turtle.color(color)
-  turtle.fillcolor(color)
-  turtle.goto(x,y)
-  turtle.pendown()
-  turtle.begin_fill()
-  turtle.circle(size)
-  turtle.end_fill()
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.circle(size)
+    turtle.end_fill()
+
 
 def draw_triangle(turtle, color, size, x, y):
-  turtle.penup()
-  turtle.color(color)
-  turtle.fillcolor(color)
-  turtle.goto(x,y)
-  turtle.pendown()
-  turtle.begin_fill()
-  for i in range (3):
-    turtle.forward(size*3)
-    turtle.left(120)
-  turtle.end_fill()
-  turtle.setheading(0)
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.begin_fill()
+    for i in range(3):
+        turtle.forward(size * 3)
+        turtle.left(120)
+    turtle.end_fill()
+    turtle.setheading(0)
+
 
 def draw_square(turtle, color, size, x, y):
-  turtle.penup()
-  turtle.color(color)
-  turtle.fillcolor(color)
-  turtle.goto(x,y)
-  turtle.pendown()
-  turtle.begin_fill()
-  for i in range (4):
-    turtle.forward(size*2)
-    turtle.left(90)
-  turtle.end_fill()
-  turtle.setheading(0)
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.begin_fill()
+    for i in range(4):
+        turtle.forward(size * 2)
+        turtle.left(90)
+    turtle.end_fill()
+    turtle.setheading(0)
+
 
 def draw_star(turtle, color, size, x, y):
-  turtle.penup()
-  turtle.color(color)
-  turtle.fillcolor(color)
-  turtle.goto(x,y)
-  turtle.pendown()
-  turtle.begin_fill()
-  turtle.right(144)
-  for i in range(5):
-    turtle.forward(size*2)
+    turtle.penup()
+    turtle.color(color)
+    turtle.fillcolor(color)
+    turtle.goto(x, y)
+    turtle.pendown()
+    turtle.begin_fill()
     turtle.right(144)
-    turtle.forward(size*2)
-  turtle.end_fill()
-  turtle.setheading(0)
+    for i in range(5):
+        turtle.forward(size * 2)
+        turtle.right(144)
+        turtle.forward(size * 2)
+    turtle.end_fill()
+    turtle.setheading(0)
