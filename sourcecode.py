ec8ee2370aad2154cd4850cacee4be646c23ae68
Shorzinator <62899599+Shorzinator@users.noreply.github.com>
2020-07-23 15:01:42 +0530

    Updated Source Code The changes made in the source were to better fit the pipeline previously imagined. Changes include - 	1. Name of store changed. 	2. Rearrangment of functions. 	3. Renaming of some functions to make them more user-understandable.

--- sourcecode.py
+++ sourcecode.py
@@ -1,37 +1,54 @@
+import time
 from turtle import *
 from shapes import *
 from menu import *
+
 
 creating_turtle()
 
 marco.penup()
 marco.goto(0, 0)
 marco.color("black")
-marco.write("Welcome to the 'Testudines Coffee'. My name is Vico and I'll be your Virtual Coffee Maker for the day. I hope your day has been pleasent so far? (Yes/No)")
+marco.write("Welcome to the 'TurCoffee'. My name is Vico and I'll be your Virtual Coffee Maker for the day. I hope your day has been pleasent so far? (Hell, Yeah! / Yes / No)")
 
 def response():
 	initial_response = input()
     if initial_response == 'Yes':
         print("So good to hear that!")
 	elif initial_response == 'Hell, Yeah!':
-		print("OMG! You sound like someone who just won a lottery! Would you be up for some recommendations to keep that energy up?")
+		print("OMG! You sound like someone who just won a lottery! Would you be up for some recommendations to keep that energy up? Or should I just display the usual menu? (R: Recommendations, U: Usual Menu)")
 		recomm_response = input()
+		if recomm_response == "R":
+			print("\n")
+			print("So, what can I make for you today, you can choose from the menu below.")
+			print("\n")
+			recommendations()
+		else:
+			print("\n")
+			print("So, what can I make for you today, you can choose from the menu below.")
+			print("\n")
+			printing_main_menu()
 	else:
         print("I hope my coffee will make you feel better")
 
-
-print("\n")
-print("So, what can I make for you today, you can choose from the menu below.")
-print("\n")
-
-printing_menu()
-
-print("Enter the number prefixed with your choice of coffee: ")
-choice_of_coffee = int(input())
-
 def recommendations():
+	print("Printing Recommended Menu", end = '')
+	time.sleep(0.5)
+	print(".", end = '')
+	time.sleep(0.5)
+	print(".", end = '')
+	time.sleep(0.5)
+	print(".", end = '')
+	time.sleep(0.5)
+	print(".", end = '')
+	time.sleep(0.5)
+	print(".", end = '')
+
+	recommended_menu()
+
 	print("Enter the number prefixed with your choice of coffee: ")
 	choice_of_coffee = int(input())
+
 	switcher_1 = {
 		1: "Potence Affagato",
 		2: "An Affair with Death",
@@ -42,7 +59,6 @@ def recommendations():
 		7: "Complex Lungo",
 		8: "Bull's Eye"			# Red Eye
 	}
-
 
 def deciding_coffee():
 	switcher_2 = {
